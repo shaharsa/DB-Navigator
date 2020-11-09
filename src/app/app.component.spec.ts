36734d9762c768d28d2000800c3d059c0c70742c
Shahar_Samira <shahar@dotbcs.com>
2020-11-09 11:18:21 +0200

    DB Navigator commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'db-navigator'`, () => {
+  it(`should have as title 'DB Navigator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('db-navigator');
+    expect(app.title).toEqual('DB Navigator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('db-navigator app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('DB Navigator app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeViewComponent } from './tree-view/tree-view.component';
+import { TabsMenuComponent } from './tabs-menu/tabs-menu.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TreeNodeService } from './tree-node.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeViewComponent,
+    TabsMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
   ],
-  providers: [],
+  providers: [
+    TreeNodeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
